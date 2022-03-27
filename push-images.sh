09eb12fddadf404394d8190c032122509a0211c4
Stuart Chinery <stuart.chinery@gmail.com>
2022-03-27 09:33:40 +0100

    6.2.3 image

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 6.2/Dockerfile -t cimg/redis:6.2.3  -t cimg/redis:6.2 .
+docker push cimg/redis:6.2.3
+docker push cimg/redis:6.2
