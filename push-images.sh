adb1e5cce94fff1e37863020fd24ac2cb4616d44
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-06-10 14:38:01 -0700

    Publish v7.0.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/redis:6.2.7
-docker push cimg/redis:6.2
-
-docker push cimg/redis:7.0.0
 docker push cimg/redis:7.0
+docker push cimg/redis:7.0.1
