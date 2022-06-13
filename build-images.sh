9978f72bedd4748f9be2180d7862c8dbf8f043dc
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-06-13 07:52:46 -0400

    Publish v7.0.1. [release] (#16)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 6.2/Dockerfile -t cimg/redis:6.2.7  -t cimg/redis:6.2 .
-docker build --file 7.0/Dockerfile -t cimg/redis:7.0.0  -t cimg/redis:7.0 .
+docker build --file 7.0/Dockerfile -t cimg/redis:7.0.1 -t cimg/redis:7.0 .
