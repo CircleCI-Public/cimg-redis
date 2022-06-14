e58c2c37058f913aa6dca62721d384701e41fd07
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-06-14 10:00:47 +0100

    Publish v7.0.2. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 7.0/Dockerfile -t cimg/redis:7.0.1 -t cimg/redis:7.0 .
+docker build --file 7.0/Dockerfile -t cimg/redis:7.0.2 -t cimg/redis:7.0 .
--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/redis:7.0
-docker push cimg/redis:7.0.1
+docker push cimg/redis:7.0.2
