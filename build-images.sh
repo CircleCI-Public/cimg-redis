0fd6fcfb5cead5a32e3e146deced138bad108a49
Jalexchen <jeff.chen@circleci.com>
2022-08-09 11:37:58 -0700

    Publish v7.0.4. [release] (#19)
    * Publish v7.0.4. [release]
    
    * [release] and update to quarterly base image

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 7.0/Dockerfile -t cimg/redis:7.0.3  -t cimg/redis:7.0 .
+docker build --file 7.0/Dockerfile -t cimg/redis:7.0.4  -t cimg/redis:7.0 .
