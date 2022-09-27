0e106c9571f6fb0992e38b83e19ab696590fc433
Jalexchen <jeff.chen@circleci.com>
2022-09-27 13:52:40 -0700

    Publish v7.0.5. [release] (#20)

--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/redis:7.0.4
+docker push cimg/redis:7.0.5
 docker push cimg/redis:7.0
