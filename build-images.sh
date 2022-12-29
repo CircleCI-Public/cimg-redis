45c0297e7605d6f41826c562d9361a54c6cb6003
Adam Hartley <git@ahartley.com>
2022-12-29 14:59:28 +0000

    Publish v7.0.7 and v6.2.8. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 7.0/Dockerfile -t cimg/redis:7.0.6  -t cimg/redis:7.0 .
+docker build --file 7.0/Dockerfile -t cimg/redis:7.0.7  -t cimg/redis:7.0 .
+docker build --file 6.2/Dockerfile -t cimg/redis:6.2.8  -t cimg/redis:6.2 .
