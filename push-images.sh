03e0bc34d371b4ef73645eec4f55b4eca751a8d7
Al Clark <alanjclark@users.noreply.github.com>
2023-03-22 11:18:35 +0000

    Publish v7.0.9. [release] (#29)

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/redis:6.0.17
-docker push cimg/redis:6.0
-
-docker push cimg/redis:6.2.10
-docker push cimg/redis:6.2
+docker push cimg/redis:7.0.9
+docker push cimg/redis:7.0
