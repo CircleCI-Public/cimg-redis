1b310f786744a8cd9f87df6f93c4dd9da4f4065d
Adam Hartley <git@ahartley.com>
2023-03-23 14:12:58 +0000

    Publish v7.0.10. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/redis:7.0.9
+docker push cimg/redis:7.0.10
 docker push cimg/redis:7.0
