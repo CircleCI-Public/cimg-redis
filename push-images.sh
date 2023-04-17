e247bb655f90a7cf9a7fda8d48455b980f22b7ef
Jeff Chen <jeff.chen@circleci.com>
2023-04-17 14:20:07 -0700

    Publish v6.0.18 and 6.2.11. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/redis:7.0.10
-docker push cimg/redis:7.0
+docker push cimg/redis:6.0
+docker push cimg/redis:6.0.18
+docker push cimg/redis:6.2
+docker push cimg/redis:6.2.11
