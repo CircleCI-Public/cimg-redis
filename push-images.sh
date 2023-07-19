7acc189dae368857d63cca2d2e39c26572406557
Adam Hartley <git@ahartley.com>
2023-07-19 14:14:29 +0100

    Test build

--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/redis:6.0
-docker push cimg/redis:6.0.20
-docker push cimg/redis:6.2
-docker push cimg/redis:6.2.13
-docker push cimg/redis:7.0
-docker push cimg/redis:7.0.12
+set -eo pipefail
